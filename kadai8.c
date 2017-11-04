5f97f67b1605eb5e277620209f6128e351b8ec7a
Tom Mas <s161078@stn.nagaokaut.ac.jp>
2017-11-05 00:38:43 +0900

    Fixed statement that asks for input number

--- kadai8.c
+++ kadai8.c
@@ -12,11 +12,11 @@ int gcd(const int a, const int b) {
 }
 
 
-inline void main(void) {
+void main(void) {
 	int a, b, c, d;					// 分数の数字
 	int numerator, denominator;		// 分子と分母
 
-	printf("\na/b + c/d を計算します。bとdには0を入力しないでください。\na = "); scanf_s("%d", &a);
+	printf("\na/b + c/d を計算します。自然数を入力してください。\na = "); scanf_s("%d", &a);
 	printf("b = "); scanf_s("%d", &b);
 	printf("c = "); scanf_s("%d", &c);
 	printf("d = "); scanf_s("%d", &d);
@@ -24,10 +24,13 @@ inline void main(void) {
 	numerator = a*d + b*c;
 	denominator = b*d;
 
-	if (denominator / gcd(numerator, denominator) == 1) {
-		printf("%d/%d + %d/%d = %d\n", a, b, c, d, numerator / gcd(numerator, denominator));
+	if (numerator == 0) {
+		printf("(%d)/(%d) + (%d)/(%d) = 0\n", a, b, c, d);
+	}
+	else if (denominator / gcd(numerator, denominator) == 1) {
+		printf("(%d)/(%d) + (%d)/(%d) = %d\n", a, b, c, d, numerator / gcd(numerator, denominator));
 	}
 	else {
-		printf("%d/%d + %d/%d = %d/%d\n", a, b, c, d, numerator / gcd(numerator, denominator), denominator / gcd(numerator, denominator));
+		printf("(%d)/(%d) + (%d)/(%d) = %d/%d\n", a, b, c, d, numerator / gcd(numerator, denominator), denominator / gcd(numerator, denominator));
 	}
 }
