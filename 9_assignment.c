17d24cd1521bc27633401d8fe1e042a5339a476e
Tom Mas <s161078@stn.nagaokaut.ac.jp>
2017-11-13 11:27:57 +0900

    9_assingment.cの整数リテラルを0と1だけに

--- 9_assignment.c
+++ 9_assignment.c
@@ -10,10 +10,12 @@ p[0]～p[9]に任意の整数を入力する。これを以下の方法で昇順(小⇒大)に並び替えて出力
 
 #include <stdio.h>
 
+#define NUM_OF_INPUT_INTEGERS 10
 
-int SortingIsFinished(int a[10]){
+
+int SortingIsFinished(int a[NUM_OF_INPUT_INTEGERS]){
 	int i;
-	for (i = 0; i < 9; i++){
+	for (i = 0; i < NUM_OF_INPUT_INTEGERS - 1; i++){
 		if (a[i] > a[i + 1]){
 			return 0;
 		}
@@ -23,18 +25,18 @@ int SortingIsFinished(int a[10]){
 
 
 void main(void){
-	printf("任意の整数を10個入力してください\n");
-	int p[10] = { 0 };
+	printf("-(2^31)～(2^31)-1の整数を10個入力してください\n");
+	int p[NUM_OF_INPUT_INTEGERS] = { 0 };
 
 	int i;
-	for (i = 0; i <= 9; i++){
+	for (i = 0; i <= NUM_OF_INPUT_INTEGERS - 1; i++){
 		scanf_s("%d", &p[i]);
 	}
 	
 	while (1){
 		int temp; // 要素交換用の一時退避所
 		int j;
-		for (j = 0; j < 9; j++){
+		for (j = 0; j < NUM_OF_INPUT_INTEGERS - 1; j++){
 			if (p[j] > p[j + 1]){
 				temp = p[j];
 				p[j] = p[j + 1];
@@ -48,7 +50,7 @@ void main(void){
 	}
 
 	int k;
-	for (k = 0; k <= 9; k++){
+	for (k = 0; k <= NUM_OF_INPUT_INTEGERS - 1; k++){
 		printf("%d ", p[k]);
 	}
 	printf("\n");
